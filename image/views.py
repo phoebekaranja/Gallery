030c2417696b5f3b7d18d6b55d2314204f108a1b
phoebekaranja <phoebemurish@gmail.com>
2019-05-12 00:18:43 +0300

    modified views files

--- image/views.py
+++ image/views.py
@@ -1,14 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http  import HttpResponse
 import datetime as dt
+from .models import Image,category,location,Photographer
+
 # Create your views here.
 global category,location
 location = ['Nairobi','Nakuru','Mombasa','Kisumu']
 category = ['Food','Clothing','Travel','Fashion']
+
 def photo_of_today(request):
     date = dt.date.today()
     photo = Image.todays_photo()
     return render(request,'all-images/today-image.html',{'date': date, 'photo':photo,'category':category})
+
 # View Function to present news from past days
 def past_days_photos(request, past_date):
 
@@ -26,6 +30,7 @@ def past_days_photos(request, past_date):
 
     photo = Image.days_photo(date)
     return render(request, 'all-images/past-image.html', {"date": date, 'photo':photo})
+
 def search_results(request):
 
     if 'image' in request.GET and request.GET["image"]:
@@ -33,11 +38,18 @@ def search_results(request):
         searched_images = Image.search_by_category(search_term)
         message = f"{search_term}"
 
-        return render(request, 'all-photos/search.html',{"message":message,"images": searched_images})
+        return render(request, 'all-images/search.html',{"message":message,"images": searched_images})
 
     else:
         message = "You haven't searched for any term"
-        return render(request, 'all-photos/search.html',{"message":message}    
+        return render(request, 'all-images/search.html',{"message":message})
+
+def category_results(request,category):
+    searched_images=Image.filter_by_category(category)
+    captions=f'{category}'
+
+    return render (request,'all-images/today-image.html',{'photo':searched_images,'captions':captions, 'category' : category})
+
 def image(request,image_id):
     try:
         image = Image.objects.get(id = image_id)
