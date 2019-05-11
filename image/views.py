1e6355e439733591d86ba5802d0805120f9b3ed5
phoebekaranja <phoebemurish@gmail.com>
2019-05-11 22:26:02 +0300

    View Function to present photos from past days

--- image/views.py
+++ image/views.py
@@ -9,3 +9,20 @@ def photo_of_today(request):
     date = dt.date.today()
     photo = Image.todays_photo()
     return render(request,'all-photos/today_rudi.html',{'date': date, 'photo':photo,'category':category})
+ View Function to present news from past days
+def past_days_photos(request, past_date):
+
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+
+    except ValueError:
+        # Raise 404 error when ValueError is thrown
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(photo_of_today)
+
+    photo = Image.days_photo(date)
+    return render(request, 'all-photos/past_rudi.html', {"date": date, 'photo':photo})
