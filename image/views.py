0fb95aac990ef11cf46da2fed1b91e6d59a709c6
phoebekaranja <phoebemurish@gmail.com>
2019-05-11 21:53:22 +0300

    first view functions

--- image/views.py
+++ image/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http  import HttpResponse
 # Create your views here.
+global category,location
+location = ['Nairobi','Nakuru','Mombasa','Kisumu']
+category = ['Food','Clothing','Travel','Fashion']
