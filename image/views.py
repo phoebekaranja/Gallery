a6ef7bf9f1acb858ec2ebef8f3bc3ae8925ad101
phoebekaranja <phoebemurish@gmail.com>
2019-05-12 00:04:20 +0300

    create the search_results view function

--- image/views.py
+++ image/views.py
@@ -26,6 +26,18 @@ def past_days_photos(request, past_date):
 
     photo = Image.days_photo(date)
     return render(request, 'all-images/past-image.html', {"date": date, 'photo':photo})
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-photos/search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'all-photos/search.html',{"message":message}    
 def image(request,image_id):
     try:
         image = Image.objects.get(id = image_id)
