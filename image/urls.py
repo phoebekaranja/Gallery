a707b55b398fae4030d63436fd14f1e476287437
phoebekaranja <phoebemurish@gmail.com>
2019-05-11 22:23:32 +0300

    created a past days url pattern

--- image/urls.py
+++ image/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
 url(r'^$',views.photo_of_today,name = 'photoToday'),
+url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
 ]
