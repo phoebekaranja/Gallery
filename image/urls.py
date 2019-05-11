df405a5a2a9715c29f44122cfaa1461e14d30bb2
phoebekaranja <phoebemurish@gmail.com>
2019-05-12 00:02:18 +0300

    added a url tag for serch results

--- image/urls.py
+++ image/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 urlpatterns=[
 url(r'^$',views.photo_of_today,name = 'photoToday'),
+url(r'^search/', views.search_results, name='search_results'),
 url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_photos,name = 'pastPhotos'),
 ]
