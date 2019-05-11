36d142274e48a57944abbf5aa9ff89f459b57fe5
phoebekaranja <phoebemurish@gmail.com>
2019-05-11 23:33:01 +0300

    created first test

--- image/tests.py
+++ image/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+from .models import Photographer,location,category,Image
 # Create your tests here.
+class PhotographerTestClass(TestCase):
+
+    # Set up method
+    def setUp(self):
+        self.james= Photographer(first_name = 'pitz', last_name ='Peter', email ='pitz@moringaschool.com')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pitz,Photographer))
