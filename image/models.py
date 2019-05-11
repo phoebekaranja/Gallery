4a2b4e57278aae2f923aebddd7cd4ae04aaed578
phoebekaranja <phoebemurish@gmail.com>
2019-05-11 23:23:44 +0300

    created a class in models file

--- image/models.py
+++ image/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+
+
+
+class Photographer(models.Model):
+    first_name = models.CharField(max_length =30)
+    last_name = models.CharField(max_length =30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.first_name
+
+    class Meta:
+        ordering = ['first_name']
+
+    def save_photography(self):
+        self.save()
